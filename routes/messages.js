3e4be4d60b04829c81082809adb0be71422941e1
Daniel Nascimento <danielhn@gmail.com>
2020-08-26 19:32:57 +0000

    Refactoring messages.js

--- routes/messages.js
+++ routes/messages.js
@@ -6,24 +6,24 @@ module.exports = (db) => {
   router.post("/", (req, res) => {
     const message = req.body;
     console.log('message =============', message.text);
-    // if (!req.session.user_id) {
-    //   res.redirect("/login")
-    // } else {
-    // console.log('!!!!!!!!!! message sent !!!!!!', messge);
-    // db.query(`INSERT INTO messages (title, description, image_url, price, user_id)
-    // VALUES ($1, $2, $3, $4,$5) RETURNING *;` ,
-    // [listing.title, listing.description, listing.image, listing.cost, req.session.user_id])
-    //   .then(data => {
-    //     const listings = data.rows;
-    //     console.log('this is data.rows!!!!!!!!!!!!!', listings)
-    //     res.redirect("/myListings");
-    //   })
-    //   .catch(err => {
-    //     res
-    //       .status(500)
-    //       .json({ error: err.message });
-    //     });
-    //   }
+    if (!req.session.user_id) {
+      res.redirect("/login")
+    } else {
+    console.log('!!!!!!!!!! message sent !!!!!!', message);
+    db.query(`INSERT INTO messages (message, user_id, listing_id, buyer_id)
+    VALUES ($1, $2, $3, $4) RETURNING *;` ,
+    [message.text, ****, req.session.user_id])
+      .then(data => {
+        const listings = data.rows;
+        console.log('this is data.rows!!!!!!!!!!!!!', listings)
+        res.redirect("/myListings");
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+        });
+      }
     });
     return router;
   }
