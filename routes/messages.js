b87c68f02ff1724aa015b75414905cc5180d0460
Devin Coughlin <devin_coughlin@hotmail.com>
2020-08-27 02:13:58 +0000

    added messaging response functionality

--- routes/messages.js
+++ routes/messages.js
@@ -3,21 +3,29 @@ const router  = express.Router();
 
 
 module.exports = (db) => {
-  router.post("/", (req, res) => {
-    const message = req.body;
-    console.log('message =============', message.text);
+  router.post("/:msgId", (req, res) => {
+    const messageId = req.params.msgId;
+    const message = req.body.text;
+    console.log('Message id======', messageId);
     if (!req.session.user_id) {
       res.redirect("/login")
     } else {
     console.log('!!!!!!!!!! message sent !!!!!!', message);
-    db.query(`INSERT INTO messages (message, user_id, listing_id, buyer_id)
-    VALUES ($1, $2, $3, $4) RETURNING *;` ,
-    [message.text, , req.session.user_id])
+      db.query(`SELECT * FROM messages WHERE id = $1;`, [messageId])
       .then(data => {
-        const listings = data.rows;
-        console.log('this is data.rows!!!!!!!!!!!!!', listings)
-        res.redirect("/myListings");
-      })
+        const msgDetails = data.rows[0];
+        console.log('this is msgDetails!!!!!!!!!!!!!', msgDetails);
+        const to = msgDetails.buyer_id
+      const from = msgDetails.user_id
+        db.query(`INSERT INTO messages (message, user_id, listing_id, buyer_id)
+          VALUES ($1, $2, $3, $4) RETURNING *;` ,
+          [message, msgDetails.user_id, msgDetails.listing_id, msgDetails.buyer_id])
+          .then(data => {
+            const response = data.rows;
+            console.log('this is response!!!!!!!!!!!!!', response)
+            res.redirect("/messages");
+          })
+        })
       .catch(err => {
         res
           .status(500)
--- server.js
+++ server.js
@@ -47,13 +47,14 @@ const registerUser = require("./routes/register_user");
 const eachListing = require("./routes/each_listing");
 const loginUser = require("./routes/login.js")
 const createListing = require("./routes/create.js");
+const messageResponses = require("./routes/messages.js")
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/register", registerUser(db));
 app.use("/create", createListing(db));
 app.use("/messages/", eachListing(db));
-
+app.use("/messagesResponses", messageResponses(db))
 // app.use("/api/listingstest", individualListing(db));
 app.use("/login", loginUser(db));
 // Note: mount other resources here, using the same pattern above
@@ -174,13 +175,16 @@ app.get("/messages", (req, res) => {
   if(!req.session.user_id) {
     res.redirect("/login");
   } else {
-    db.query(`SELECT messages.*, listings.*, users.username AS buyer_name FROM messages JOIN users ON users.id = buyer_id
+    db.query(`SELECT messages.*, messages.id AS message_id, listings.*, users.username AS buyer_name
+    FROM messages
+    JOIN users ON users.id = buyer_id
     JOIN listings ON users.id = listings.user_id
-    WHERE messages.user_id = $1 OR buyer_id = $2;`,
+    WHERE messages.user_id = $1 OR buyer_id = $2
+    ORDER BY messages.id DESC;`,
     [req.session.user_id, req.session.user_id])
     .then((data) => {
       const listOfMessages = data.rows;
-      console.log('MESSAGES DATA:' , data.rows[0])
+      console.log('LIST OF MESSAGES ', listOfMessages)
       console.log(req.session.username)
       let templateVars =  { username : req.session.username, listOfMessages };
       res.render("messages", templateVars);
@@ -194,3 +198,7 @@ app.get("/messages", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+
+
+// $(".messageForm").each(function(){console.log($(this).attr("action"))})
