4f98697319bb22dd836c1a1a0b52afe6ef20dd19
Daniel Nascimento <danielhn@gmail.com>
2020-08-27 17:50:40 +0000

    Rebuild database

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -4,13 +4,29 @@ INSERT INTO users (username, email, password, phone_number)
 VALUES ('Daniel', 'daniel@email.com', '1', '647-616-6160');
 
 INSERT INTO users (username, email, password, phone_number)
-VALUES ('Monica', 'monica@email.com', '1', '647-555-6160');
+VALUES ('Monica', 'monica@email.com', '1', '647-555-6161');
 
 INSERT INTO users (username, email, password, phone_number)
-VALUES ('Devin', 'devin@email.com', '1', '647-647-7469');
+VALUES ('Devin', 'devin@email.com', '1', '647-647-7461');
 
 INSERT INTO users (username, email, password, phone_number)
 VALUES ('Mario', 'mario@email.com', '1', '123-456-7899');
 
 INSERT INTO users (username, email, password, phone_number)
-VALUES ('Luigi', 'luigi@email.com', '1', '987-654-3512');
+VALUES ('Luigi', 'luigi@email.com', '1', '937-654-3512');
+
+INSERT INTO users (username, email, password, phone_number)
+VALUES ('Yoshi', 'yoshi@email.com', '1', '957-677-3512');
+
+INSERT INTO users (username, email, password, phone_number)
+VALUES ('Tails', 'tails@email.com', '1', '687-654-3446');
+
+INSERT INTO users (username, email, password, phone_number)
+VALUES ('Spyro', 'spyro@email.com', '1', '587-654-3556');
+
+INSERT INTO users (username, email, password, phone_number)
+VALUES ('Gabigol', 'gabigol@email.com', '1', '487-654-7516');
+
+INSERT INTO users (username, email, password, phone_number)
+VALUES ('Vampeta', 'vampeta@email.com', '1', '787-666-3516');
+
